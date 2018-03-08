8472da2930d3d296a8f0d410c48bc18eac8e47b2
szhqx123 <377508779@qq.com>
2018-03-08 13:50:59 +0800

    wx config

--- api/config/params.php
+++ api/config/params.php
@@ -4,22 +4,22 @@ return [
     // token 有效期默认1天
     'user.apiTokenExpire' => 1*24*3600,
     'WECHAT' => [
-        'APPID' => 'wx9c044f98156b8e20',
-        'APPSECRET' => '3fd73bb4cd76af92528e3c393435e2ab',
+        'APPID' =>  env('APP_ID','wx9c044f98156b8e20'),
+        'APPSECRET' => env('APP_SECRET','3fd73bb4cd76af92528e3c393435e2ab'),
     ],
-    'DEFAULT_TARGET_URL' => 'http://localhost:8300/stopCar',
+    'DEFAULT_TARGET_URL' =>env('DEFAULT_TARGET_URL','http://localhost:8300/stopCar'),
     'FREE_TIMES' => 3, //首个微信用户的免费次数
     'DISTANCE' => 500, //默认距离（米）
 
     'WX_PAY' => [
-        'APP_ID' => 'wx9c044f98156b8e20',
-        'APP_SECRET' => '3fd73bb4cd76af92528e3c393435e2ab',
-        'WX_PAY_APP_ID' => 'wx9c044f98156b8e20',
-        'WX_PAY_APP_SECRET' => '3fd73bb4cd76af92528e3c393435e2ab',
-        'WX_PAY_MERCHANT_ID' => '1355373002',
-        'WX_PAY_KEY_FOR_SIGN' => 'l2hq3qtidf11kza2hqvmhtwdvp5r4xp4',
-        'WX_CERT_PATH' => '/home/deploy/check_in_backend/pay/apiclient_cert.pem',
-        'WX_KEY_PATH' => '/home/deploy/check_in_backend/pay/apiclient_key.pem',
-        'PAY_NOTIFY_URL' => '/index.php?r=activity/notify',
+        'APP_ID' => env('APP_ID','wx064cd93da2e5faed'),
+        'APP_SECRET' => env('APP_SECRET','04b01064c6c584541f64b2280fba8a56'),
+        'WX_PAY_APP_ID' =>  env('WX_PAY_APP_ID','wx9c044f98156b8e20'),
+        'WX_PAY_APP_SECRET' =>  env('WX_PAY_APP_SECRET','wx9c044f98156b8e20'),
+        'WX_PAY_MERCHANT_ID' => env('WX_PAY_MERCHANT_ID','wx9c044f98156b8e20'),
+        'WX_PAY_KEY_FOR_SIGN' => env('WX_PAY_KEY_FOR_SIGN','wx9c044f98156b8e20'),
+        'WX_CERT_PATH' =>  env('WX_CERT_PATH','/home/deploy/check_in_backend/pay/apiclient_cert.pem'),
+        'WX_KEY_PATH' =>  env('WX_KEY_PATH','/home/deploy/check_in_backend/pay/apiclient_key.pem'),
+        'PAY_NOTIFY_URL' => env('PAY_NOTIFY_URL','/index.php?r=activity/notify'),
     ]
 ];
