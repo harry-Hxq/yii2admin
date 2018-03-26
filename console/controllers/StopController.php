fda255accf6505f48cdaeb881b07b53e15f472ea
szhqx123 <377508779@qq.com>
2018-03-26 11:57:57 +0800

    fix

--- console/controllers/StopController.php
+++ console/controllers/StopController.php
@@ -13,13 +13,15 @@ class StopController extends Controller
 {
     /*
      * ---------------------------------------
-     *
+     * 更新会员状态（将过期的用户标记未已过期）
      * ---------------------------------------
      */
-    public function actionClear()
+    public function actionUpdateUser()
     {
-        Picture::clearPic(Yii::$app->params['upload']['path']);
-        return static::EXIT_CODE_NORMAL;
+        $db = Yii::$app->db;
+        $now = strtotime(date("Y-m-d")) - 86400;
+        $raw_sql = sprintf("update yii2_user set deadline = %d WHERE deadline = %d;",0, $now);
+        $db->createCommand($raw_sql)->execute();
     }
 
 }
